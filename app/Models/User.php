0d514566e2743f9a1fc3a7c930009ee571ffc25e
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-06 03:50:28 +0200

    Task summaty

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -52,4 +53,12 @@ class User extends Authenticatable
     {
         return $this->hasMany(Task::class);
     }
+
+    public function tasksSummary(): Collection
+{
+    return $this->tasks()
+        ->where('created_at', '>=', now()->subWeek())
+        ->latest()
+        ->get();
+}
 }
