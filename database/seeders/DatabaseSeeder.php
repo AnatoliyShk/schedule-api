936c99ec521161f993eda3cff52ab7c7349bbd2b
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-09 23:23:51 +0200

    task seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,11 +21,14 @@ class DatabaseSeeder extends Seeder
                 foreach ($period as $date) {
                     $date->hour(rand(0, 23))->minute(rand(0, 6) * 10);
 
-                    Task::factory()->create([
-                        'user_id' => $user->id,
-                        'created_at' => $date,
-                        'updated_at' => $date
-                    ]);
+                    Task::factory()
+                        ->withRandomPriority()
+                        ->withRandomDueDate()
+                        ->create([
+                            'user_id' => $user->id,
+                            'created_at' => $date,
+                            'updated_at' => $date
+                        ]);
                 }
             });
     }
