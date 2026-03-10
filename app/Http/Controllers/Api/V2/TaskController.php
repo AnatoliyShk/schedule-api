72f1e58482c2f037c00dda5f55e8bdd1017cef6e
Anatoliy Shekera <tolikshekera@gmail.com>
2026-03-10 23:35:51 +0200

    Parser tests

--- app/Http/Controllers/Api/V2/TaskController.php
+++ app/Http/Controllers/Api/V2/TaskController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Api\V2;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
-use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Log;
+use App\Parsers\TaskInputParser;
 
 class TaskController extends Controller
 {
+    public function __construct(protected TaskInputParser $parser)
+    {
+    }
+
     public function index()
     {
         Gate::authorize('viewAny', Task::class);
@@ -34,17 +37,26 @@ class TaskController extends Controller
     public function store(StoreTaskRequest $request)
     {
         Gate::authorize('create', Task::class);
-        $task = $request->user()->tasks()->create($request->validated());
+
+        $data = $request->validated();
+        $task = $request->user()->tasks()->create(
+            $this->prepareData($data)
+        );
+
         $task->load('priority');
+
         return $task->toResource();
     }
 
     public function update(UpdateTaskRequest $request, Task $task)
     {
-
         Gate::authorize('update', $task);
-        $task->update($request->validated());
+
+        $task->update(
+            $this->prepareData($request->validated())
+        );
         $task->load('priority');
+
         return $task->toResource();
     }
 
@@ -53,6 +65,17 @@ class TaskController extends Controller
         Gate::authorize('delete', $task);
         $task->delete();
         return response()->noContent();
+    }
+
+    private function prepareData(array $data): array
+    {
+        $parsed = $this->parser->parse($data['name']);
+        if ($parsed) {
+            $data['name'] = $parsed['name'];
+            $data['priority_id'] = $data['priority_id'] ?? ($parsed['priority_id'] ?? null);
+            $data['due_date'] = $data['due_date'] ?? ($parsed['due_date'] ?? null);
+        }
+        return $data;
     }
 }
 
